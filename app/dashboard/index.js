9bb236f057ae1b49f1e822f262dc085c7d7216f4
Cléber Otaviano <cleberotaviano.core@gmail.com>
2018-06-14 21:00:31 -0300

    Historico de chamado

--- app/dashboard/index.js
+++ app/dashboard/index.js
@@ -11,13 +11,12 @@ function dashboard(app) {
         })
     });
     app.get('/dashboard/detalhe-chamado/:idchamado', function(req, res) {
-
-        // let isLogado = req.user ? true : false;
-
         res.render('dashboard/detalhe-chamado/index', {
-            // isLogado,
-            // notLayoutMenu: true,
-            // invertedMenu: true
+            teste: 'Detalhe chamado POLA'
+        })
+    });
+    app.get('/dashboard/historico-chamado', function(req, res) {
+        res.render('dashboard/historico-chamado/index', {
             teste: 'Detalhe chamado POLA'
         })
     });
